9197b80f1c474a0f84a50fb5bcab856b420b7fb6
mshuktuev <travokur321@yandex.ru>
2023-10-06 18:03:33 +0500

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,23 @@
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
-export default {
-  input: "src/index.ts",
-  plugins: [terser(), typescript()],
-  output: {
-    file: "dist/index.module.js",
-    format: "esm",
+export default [
+  {
+    input: "src/index.ts",
+    plugins: [terser(), typescript()],
+    external: ["three", "three/examples/jsm/controls/OrbitControls"],
+    output: {
+      file: "dist/index.module.js",
+      format: "esm",
+    },
   },
-};
+  {
+    input: "src/index.ts",
+    plugins: [terser(), typescript()],
+    external: ["three", "three/examples/jsm/controls/OrbitControls"],
+    output: {
+      file: "dist/index.module.cjs",
+      format: "cjs",
+    },
+  },
+];
