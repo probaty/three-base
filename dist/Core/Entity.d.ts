cd22404c2d96675df8d057cf9edb58c721bd7131
mshuktuev <travokur321@yandex.ru>
2023-10-06 15:46:14 +0500

    upd dist

--- dist/Core/Entity.d.ts
+++ dist/Core/Entity.d.ts
@@ -1,9 +1,12 @@
 import { EntityEvent } from "../types/eventsTypes";
 import { ComponentBase } from "./Component";
+import * as THREE from "three";
 export declare class Entity extends ComponentBase {
     private _children;
     private _name;
     private _handlers;
+    private _position;
+    private _rotation;
     constructor();
     AddChild(child: ComponentBase): void;
     RemoveChild(child: ComponentBase): void;
@@ -15,4 +18,8 @@ export declare class Entity extends ComponentBase {
     AddHandler(type: string, handler: EntityEvent): void;
     get Name(): string | null;
     set Name(name: string);
+    get Position(): THREE.Vector3;
+    set Position(position: THREE.Vector3);
+    set Rotation(rotation: THREE.Euler);
+    get Rotation(): THREE.Euler;
 }
--- dist/Core/Entity.js
+++ dist/Core/Entity.js
@@ -1,13 +1,39 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Entity = void 0;
 const Component_1 = require("./Component");
+const THREE = __importStar(require("three"));
 class Entity extends Component_1.ComponentBase {
     constructor() {
         super();
         this._children = [];
         this._name = null;
         this._handlers = {};
+        this._position = new THREE.Vector3();
+        this._rotation = new THREE.Euler();
     }
     AddChild(child) {
         child.Parent = this;
@@ -46,6 +72,20 @@ class Entity extends Component_1.ComponentBase {
     }
     set Name(name) {
         this._name = name;
+    }
+    get Position() {
+        return this._position;
+    }
+    set Position(position) {
+        this._position.copy(position);
+        this.Emit("update.position", this._position);
+    }
+    set Rotation(rotation) {
+        this._rotation.copy(rotation);
+        this.Emit("update.rotation", this._rotation);
+    }
+    get Rotation() {
+        return this._rotation;
     }
 }
 exports.Entity = Entity;
