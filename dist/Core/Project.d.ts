0303427eb661bdedb390ee464cd164f24de23fa1
mshuktuev <travokur321@yandex.ru>
2023-10-06 17:41:52 +0500

    upd build

--- dist/Core/Project.d.ts
+++ dist/Core/Project.d.ts
@@ -1,23 +1,22 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { Entity } from "./Entity";
-import { EntityController } from "./EntityController";
 interface ProjectOptions {
     useOrbitControls?: boolean;
 }
 export declare class Project {
     options: ProjectOptions;
-    _rootElement: HTMLElement;
-    _renderer: THREE.WebGLRenderer;
-    _scene: THREE.Scene;
-    _camera: THREE.PerspectiveCamera;
-    _orbitControls: OrbitControls | null;
-    _entityController: EntityController;
-    _prevTime: number | null;
+    private _rootElement;
+    private _renderer;
+    private _scene;
+    private _camera;
+    private _orbitControls;
+    private _entityController;
+    private _prevTime;
     constructor(rootElement: HTMLElement | string, options?: ProjectOptions);
     private createRenderer;
     private createCamera;
     private createScene;
+    private onWindowResize;
     AddChild(child: Entity, name?: string): void;
     private removeChild;
     private update;
