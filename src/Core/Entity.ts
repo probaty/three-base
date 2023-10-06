bb06cbca5dbdc5bc0780580fce53620879c48203
mshuktuev <travokur321@yandex.ru>
2023-10-06 15:44:44 +0500

    added pos and rotation properties to Entity class

--- src/Core/Entity.ts
+++ src/Core/Entity.ts
@@ -1,11 +1,13 @@
 import { EntityEvent } from "../types/eventsTypes";
-import { Component, ComponentBase } from "./Component";
-import { EntityController } from "./EntityController";
+import { ComponentBase } from "./Component";
+import * as THREE from "three";
 
 export class Entity extends ComponentBase {
   private _children: ComponentBase[] = [];
   private _name: string | null = null;
   private _handlers: { [name: string]: EntityEvent[] } = {};
+  private _position: THREE.Vector3 = new THREE.Vector3();
+  private _rotation: THREE.Euler = new THREE.Euler();
 
   constructor() {
     super();
@@ -54,5 +56,23 @@ export class Entity extends ComponentBase {
 
   set Name(name: string) {
     this._name = name;
+  }
+
+  get Position(): THREE.Vector3 {
+    return this._position;
+  }
+
+  set Position(position: THREE.Vector3) {
+    this._position.copy(position);
+    this.Emit("update.position", this._position);
+  }
+
+  set Rotation(rotation: THREE.Euler) {
+    this._rotation.copy(rotation);
+    this.Emit("update.rotation", this._rotation);
+  }
+
+  get Rotation(): THREE.Euler {
+    return this._rotation;
   }
 }
